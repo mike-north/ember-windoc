4a7b3ea3e8e404a57a7a906bc0e7febb99300610
Mike North <michael.l.north@gmail.com>
2018-11-10 09:39:17 -0800

    fix: linting issues

--- tests/acceptance/index-test.js
+++ tests/acceptance/index-test.js
@@ -6,12 +6,12 @@ moduleForAcceptance('Acceptance | index');
 test('visiting /index', function(assert) {
   visit('/').then(function() {
     assert.equal(currentURL(), '/');
-    assert.equal(find('.scroll-height').text(), `${document.body.scrollHeight}`, 'Correct scrollHeight');
-    assert.equal(find('.scroll-width').text(), `${document.body.scrollWidth}`, 'Correct scrollWidth');
+    assert.equal(document.querySelector('.scroll-height').innerText.trim(), `${document.body.scrollHeight}`, 'Correct scrollHeight');
+    assert.equal(document.querySelector('.scroll-width').innerText.trim(), `${document.body.scrollWidth}`, 'Correct scrollWidth');
     let lastScrollB = (document.body.scrollHeight - document.documentElement.clientHeight) - document.documentElement.scrollTop;
-    assert.equal(find('.scroll-bottom').text(), `${lastScrollB}`, 'Correct scrollBottom');
-    assert.equal(find('.scroll-right').text(), `${(document.body.scrollWidth - document.documentElement.clientWidth) - document.documentElement.scrollLeft}`, 'Correct scrollRight');
-    assert.equal(find('.scroll-top').text(), `${document.body.scrollTop}`, 'Correct scrollTop');
+    assert.equal(document.querySelector('.scroll-bottom').innerText.trim(), `${lastScrollB}`, 'Correct scrollBottom');
+    assert.equal(document.querySelector('.scroll-right').innerText.trim(), `${(document.body.scrollWidth - document.documentElement.clientWidth) - document.documentElement.scrollLeft}`, 'Correct scrollRight');
+    assert.equal(document.querySelector('.scroll-top').innerText.trim(), `${document.body.scrollTop}`, 'Correct scrollTop');
   });
 
 });
